d91f98adc91082e37904312e2158c229c26ec1f4
Lakshan Rajasuriya <lakshanrajasuriya@gmail.com>
2021-06-02 18:38:24 +0530

    feat: add fibonacci-sequence

---
+++ fibonacci-sequence/src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  number: number = 0; // To check fibonacci sequence
+  message: string = ''; // To print the message
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  findFibonacci() {
+
+    let sequence = [0]; // To store fibonacci sequence
+    let index = 0; // To get the current index of sequence
+
+    while (this.number > sequence[index]) {
+
+      if (sequence[index] >= 1) {
+        sequence.push(sequence[index] + (sequence[index - 1]));
+      } else {
+        sequence.push(sequence[index] + 1);
+      }
+
+      index++;
+    }
+
+    if (sequence[index] == this.number)
+      this.message = index + ' index in fibonacci sequence'
+    else
+      this.message = 'Not in fibonacci sequence!'
+  }
+}
